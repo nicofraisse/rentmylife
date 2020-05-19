7abd0e93b595f71ed98f1d5c18679eed739011e4
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-19 11:01:09 -0400

    images & lifestyle page fixed, seeds finished"

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :lifestyle
-
   validates :comment, length: { minimum: 20 }
 end
--- db/migrate/20190822142954_add_photo_to_lifestyles.rb
+++ db/migrate/20190822142954_add_photo_to_lifestyles.rb
@@ -1,3 +1,4 @@
+
 class AddPhotoToLifestyles < ActiveRecord::Migration[5.2]
   def change
     add_column :lifestyles, :photo, :string
